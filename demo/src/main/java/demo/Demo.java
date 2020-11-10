be8fab2994630a7bcc559c593534267a66e12c66
1093357043 <73806828+1093357043@users.noreply.github.com>
2020-11-10 15:25:06 +0800

    .

--- demo/src/main/java/demo/Demo.java
+++ demo/src/main/java/demo/Demo.java
@@ -1,6 +1,6 @@
 package demo;
 
-import com.qq.weixin.api.WeixinAPI;
+import cn.onekit.thekit.JSON;
 import com.qq.weixin.api.entity.*;
 import com.qq.weixin.api.sdk.WeixinSDK;
 import org.springframework.util.ResourceUtils;
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.crypto.Data;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 @RestController
@@ -73,8 +76,8 @@ public class Demo {
     public wxa__media_check_async_response mediaCheckAsync(
             @RequestParam String access_token) throws Exception {
         wxa__media_check_async_body body = new wxa__media_check_async_body();
-        //body.setMedia_type(2);
-        body.setMedia_url("");
+        body.setMedia_type(2);
+        body.setMedia_url("#");
         return new WeixinSDK().wxa__media_check_async(access_token,body);
 
 
@@ -84,7 +87,7 @@ public class Demo {
     public WeixinResponse msgSecCheck(
             @RequestParam String access_token) throws Exception {
         wxa__msg_sec_check_body body = new wxa__msg_sec_check_body();
-        body.setContent("");
+        body.setContent("xx");
         return new WeixinSDK().wxa__msg_sec_check(access_token,body);
 
     }
@@ -93,11 +96,13 @@ public class Demo {
     public WeixinResponse removeUserStorage(
             @RequestParam String access_token,
             @RequestParam String openid,
-            @RequestParam String session_key,
-            @RequestParam String data) throws Exception {
-        String signature = new WeixinSDK()._crypto(sig_method, session_key,data);
+            @RequestParam String session_key) throws Exception {
+
         wxa__remove_user_storage_body body = new wxa__remove_user_storage_body();
-        //body.setKey("");
+
+        body.setKey(new ArrayList<String>(){{add("key1");}});
+        String signature = new WeixinSDK()._crypto(sig_method, session_key, JSON.object2string(body));
+
         return new WeixinSDK().wxa__remove_user_storage(access_token,openid,signature,sig_method,body);
 
     }
@@ -106,17 +111,112 @@ public class Demo {
     public WeixinResponse setUserInteractiveData(
             @RequestParam String access_token,
             @RequestParam String openid,
-            @RequestParam String session_key,
-            @RequestParam String data) throws Exception {
-        String signature = new WeixinSDK()._crypto(sig_method, session_key,data);
-
-
+            @RequestParam String session_key) throws Exception {
         wxa__setuserinteractivedata_body body = new wxa__setuserinteractivedata_body();
-        //body.setKv_list();
+
+        body.setKv_list(new ArrayList<KV>(){{add(new KV("key1","value1"));}});
+        String signature = new WeixinSDK()._crypto(sig_method, session_key,JSON.object2string(body));
+
         return new WeixinSDK().wxa__setuserinteractivedata(access_token,openid,signature,sig_method,body);
 
     }
 
+    @RequestMapping("/setUserStorage")
+    public WeixinResponse setUserStorage(
+            @RequestParam String access_token,
+            @RequestParam String openid,
+            @RequestParam String session_key) throws Exception {
+        wxa__set_user_storage_body body = new wxa__set_user_storage_body();
 
+        body.setKv_list(new ArrayList<KV>(){{add(new KV("key1","value1"));}});
+        String signature = new WeixinSDK()._crypto(sig_method, session_key,JSON.object2string(body));
+
+        return new WeixinSDK().wxa__set_user_storage(access_token,openid,signature,sig_method,body);
+
+    }
+
+    @RequestMapping("/createActivityId")
+    public cgi_bin__message__wxopen__activityid__create_response createActivityId(
+            @RequestParam String access_token,
+            @RequestParam String unionid) throws Exception {
+        return new WeixinSDK().cgi_bin__message__wxopen__activityid__create(access_token,unionid);
+
+    }
+
+    @RequestMapping("/setUpdatableMsg")
+    public WeixinResponse setUpdatableMsg(
+            @RequestParam String access_token) throws Exception {
+        updatablemsg__send_body body = new updatablemsg__send_body();
+        body.setActivity_id("xxx");
+        body.setTarget_state(1);
+        Parameter parameter = new Parameter();
+        parameter.setName("name1");
+        parameter.setValue("value1");
+        body.setTemplate_info(new ArrayList<Parameter>(){{add(parameter);}});
+        return new WeixinSDK().cgi_bin__message__wxopen__updatablemsg__send(access_token,body);
+
+    }
+
+    @RequestMapping("/createQRCode")
+    public cgi_bin__wxaapp__createwxaqrcode_response createQRCode(
+            @RequestParam String access_token) throws Exception {
+        wxaapp__createwxaqrcode_body body = new wxaapp__createwxaqrcode_body();
+        body.setPath("xx");
+        body.setWidth(500);
+        return new WeixinSDK().cgi_bin__wxaapp__createwxaqrcode(access_token,body);
+
+    }
+
+    @RequestMapping("/get")
+    public WeixinResponse get(
+            @RequestParam String access_token) throws Exception {
+        wxa__getwxacode_body body = new wxa__getwxacode_body();
+        body.setPath("xx");
+        body.setWidth(500);
+        body.setAuto_color(false);
+        body.setIs_hyaline(false);
+        GRB grb = new GRB();
+        grb.setG(0);
+        grb.setR(0);
+        grb.setB(0);
+        body.setLine_color(grb);
+        return new WeixinSDK().wxa__getwxacode(access_token,body);
+
+    }
+
+    @RequestMapping("/getUnlimited")
+    public wxa__getwxacodeunlimit_response getUnlimited(
+            @RequestParam String access_token) throws Exception {
+        wxa__getwxacodeunlimit_body body = new wxa__getwxacodeunlimit_body();
+        body.setScene("xxxxxxx");
+        body.setPage("pages/index/index");
+        body.setWidth(111111);
+        body.setAuto_color(false);
+        GRB grb = new GRB();
+        grb.setG(0);
+        grb.setR(0);
+        grb.setB(0);
+        body.setLine_color(grb);
+        body.setIs_hyaline(false);
+        return new WeixinSDK().wxa__getwxacodeunlimit(access_token,body);
+
+    }
+
+    @RequestMapping("/send")
+    public WeixinResponse send(
+            @RequestParam String access_token) throws Exception {
+        subscribe__send_body body = new subscribe__send_body();
+        body.setTouser("xxx");
+        body.setTemplate_id("xxx");
+        body.setPage("xxx");
+        subscribe__send_body.Data.DataValue dataValue = new subscribe__send_body.Data.DataValue();
+        dataValue.setValue("xxx");
+
+        body.setData(new HashMap<String, subscribe__send_body.Data.DataValue>(){{put("xxx",dataValue);}});
+        body.setMiniprogram_state("formal");
+        body.setLang("zh_CN");
+
+        return new WeixinSDK().cgi_bin__message__subscribe__send(access_token,body);
+    }
 
 }
