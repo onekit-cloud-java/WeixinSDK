faec35f4ae3188d521c5291ab945355667b51100
your name <your email>
2020-11-15 22:48:22 +0800

    .

--- src/main/java/com/qq/weixin/api/WeixinSDK.java
+++ src/main/java/com/qq/weixin/api/WeixinSDK.java
@@ -12,6 +12,11 @@ import java.util.HashMap;
 
 public class WeixinSDK implements WeixinAPI {
 
+    private final String host;
+
+    public WeixinSDK(String host){
+        this.host=host;
+    }
     public String _decrypt(String wx_encryptedData,String wx_iv,String wx_session_key) throws Exception {
         return new CRYPTO(CRYPTO.Key.AES, CRYPTO.Mode.PKCS5, 128).decrypt(wx_encryptedData, wx_iv, wx_session_key);
     }
@@ -19,7 +24,7 @@ public class WeixinSDK implements WeixinAPI {
     public cgi_bin__token_response cgi_bin__token(String wx_appid, String wx_secret, String wx_grant_type) throws WeixinError{
         JsonObject result;
         try {
-            String url = "https://api.weixin.qq.com/cgi-bin/token";
+            String url = String.format("%s/cgi-bin/token",host);
             result = (JsonObject) JSON.parse (AJAX.request(url, "get", new HashMap<String,String>(){{
                 put("appid", wx_appid);
                 put("secret", wx_secret);
@@ -39,7 +44,7 @@ public class WeixinSDK implements WeixinAPI {
     public WeixinResponse wxa__checksession(String wx_access_token,String openid,String signature,String sig_method,String wx_body) {
         JsonObject result;
         try {
-            String url = String.format("https://api.weixin.qq.com/wxa/checksession?access_token=%s&openid=%s&signature=%s&sig_method=%s",wx_access_token,openid,signature,sig_method);
+            String url = String.format("%s/wxa/checksession?access_token=%s&openid=%s&signature=%s&sig_method=%s",host,wx_access_token,openid,signature,sig_method);
             result = (JsonObject) JSON.parse(AJAX.request(url,"POST", wx_body));
         } catch (Exception e) {
             cgi_bin__token_response error = new cgi_bin__token_response();
@@ -56,7 +61,7 @@ public class WeixinSDK implements WeixinAPI {
     public snc__jscode2session_response snc__jscode2session(String wx_appid,String wx_secret,String wx_js_code,String wx_grant_type) {
         JsonObject result ;
         try {
-            String url = "https://api.weixin.qq.com/sns/jscode2session";
+            String url = String.format("%s/sns/jscode2session",host);
             result  =(JsonObject) JSON.parse(AJAX.request(url,"get",new HashMap<String, String>(){{
                 put("appid", wx_appid);
                 put("secret", wx_secret);
@@ -80,7 +85,7 @@ public class WeixinSDK implements WeixinAPI {
     public WeixinResponse wxa__img_sec_check(String wx_access_token,byte[] wx_body) {
         JsonObject reuslt;
         try {
-            String url = String.format("https://api.weixin.qq.com/wxa/img_sec_check?access_token=%s",wx_access_token);
+            String url = String.format("%s/wxa/img_sec_check?access_token=%s",host,wx_access_token);
             reuslt = (JsonObject) JSON.parse(AJAX.upload(url,wx_body));
         } catch (Exception e) {
             snc__jscode2session_response error = new snc__jscode2session_response();
@@ -95,7 +100,7 @@ public class WeixinSDK implements WeixinAPI {
     public wxa__media_check_async_response wxa__media_check_async(String wx_access_token,wxa__media_check_async_body wx_body) {
         JsonObject result;
         try {
-            String url = String.format("https://api.weixin.qq.com/wxa/media_check_async?access_token=%s",wx_access_token);
+            String url = String.format("%s/wxa/media_check_async?access_token=%s",host,wx_access_token);
             JsonObject post_body = (JsonObject) JSON.object2json(wx_body);
             result = (JsonObject) JSON.parse(AJAX.request(url,"post",post_body.toString()));
         } catch (Exception e) {
@@ -111,7 +116,7 @@ public class WeixinSDK implements WeixinAPI {
     public WeixinResponse wxa__msg_sec_check(String wx_access_token, wxa__msg_sec_check_body wx_body) {
         JsonObject result;
         try {
-            String url = String.format("https://api.weixin.qq.com/wxa/msg_sec_check?access_token=%s",wx_access_token);
+            String url = String.format("%s/wxa/msg_sec_check?access_token=%s",host,wx_access_token);
             JsonObject post_body = (JsonObject) JSON.object2json(wx_body);
             result = (JsonObject) JSON.parse(AJAX.request(url,"post",post_body.toString()));
         } catch (Exception e) {
@@ -128,8 +133,8 @@ public class WeixinSDK implements WeixinAPI {
     public WeixinResponse wxa__remove_user_storage(String wx_access_token, String openid, String signature, String sig_method, wxa__remove_user_storage_body wx_body) {
         JsonObject result;
         try {
-            String url = String.format("https://api.weixin.qq.com/wxa/remove_user_storage?access_token=%s&signature=%s&openid=%s&sig_method=%s",
-                    wx_access_token,openid,signature,sig_method);
+            String url = String.format("%s/wxa/remove_user_storage?access_token=%s&signature=%s&openid=%s&sig_method=%s",
+                    host,wx_access_token,openid,signature,sig_method);
             JsonObject post_body = (JsonObject) JSON.object2json(wx_body);
             result = (JsonObject) JSON.parse(AJAX.request(url,"post",post_body.toString()));
 
@@ -146,8 +151,8 @@ public class WeixinSDK implements WeixinAPI {
     public WeixinResponse wxa__setuserinteractivedata(String wx_access_token, String openid, String signature, String sig_method, wxa__setuserinteractivedata_body wx_body) {
         JsonObject result;
         try {
-            String url = String.format("https://api.weixin.qq.com/wxa/setuserinteractivedata?access_token=%s&signature=%s&openid=%s&sig_method=%s",
-                    wx_access_token,signature,openid,sig_method);
+            String url = String.format("%s/wxa/setuserinteractivedata?access_token=%s&signature=%s&openid=%s&sig_method=%s",
+                    host,wx_access_token,signature,openid,sig_method);
             JsonObject post_body = (JsonObject) JSON.object2json(wx_body);
             result = (JsonObject) JSON.parse(AJAX.request(url,"post",post_body.toString()));
         } catch (Exception e) {
@@ -163,8 +168,8 @@ public class WeixinSDK implements WeixinAPI {
     public WeixinResponse wxa__set_user_storage(String wx_access_token, String openid, String signature, String sig_method, wxa__set_user_storage_body wx_body) {
         JsonObject result;
         try {
-            String url = String.format("https://api.weixin.qq.com/wxa/set_user_storage?access_token=%s&signature=%s&openid=%s&sig_method=%s",
-                    wx_access_token,signature,openid,sig_method);
+            String url = String.format("%s/wxa/set_user_storage?access_token=%s&signature=%s&openid=%s&sig_method=%s",
+                    host,wx_access_token,signature,openid,sig_method);
             JsonObject post_body = (JsonObject) JSON.object2json(wx_body);
             result = (JsonObject) JSON.parse(AJAX.request(url,"post",post_body.toString()));
         } catch (Exception e) {
@@ -180,7 +185,7 @@ public class WeixinSDK implements WeixinAPI {
     public cgi_bin__message__wxopen__activityid__create_response cgi_bin__message__wxopen__activityid__create(String wx_access_token, String unionid) {
         JsonObject result;
         try {
-            String url = "https://api.weixin.qq.com/cgi-bin/message/wxopen/activityid/create";
+            String url = String.format("%s/cgi-bin/message/wxopen/activityid/create",host);
             result = (JsonObject) JSON.parse(AJAX.request(url,"get",new HashMap<String, String>(){{
                 put("access_token",wx_access_token);
                 put("unionid",unionid);
@@ -197,7 +202,7 @@ public class WeixinSDK implements WeixinAPI {
     public WeixinResponse cgi_bin__message__wxopen__updatablemsg__send(String wx_access_token,updatablemsg__send_body wx_body) {
         JsonObject result;
         try {
-            String url = String.format("https://api.weixin.qq.com/wxa/set_user_storage?access_token=%s",wx_access_token);
+            String url = String.format("%s/wxa/set_user_storage?access_token=%s",host,wx_access_token);
             JsonObject post_body = (JsonObject) JSON.object2json(wx_body);
             result = (JsonObject) JSON.parse(AJAX.request(url,"post",post_body.toString()));
         } catch (Exception e) {
@@ -213,7 +218,7 @@ public class WeixinSDK implements WeixinAPI {
     public byte[] cgi_bin__wxaapp__createwxaqrcode(String wx_access_token,wxaapp__createwxaqrcode_body wx_body) throws WeixinError {
 
         try {
-            String url = String.format("https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token=%s",wx_access_token);
+            String url = String.format("%s/cgi-bin/wxaapp/createwxaqrcode?access_token=%s",host,wx_access_token);
             JsonObject post_body = (JsonObject) JSON.object2json(wx_body);
             return AJAX.download(url,"post",post_body.toString());
         } catch (Exception e) {
@@ -227,7 +232,7 @@ public class WeixinSDK implements WeixinAPI {
     @Override
     public byte[] wxa__getwxacode(String wx_access_token,wxa__getwxacode_body wx_body) throws WeixinError{
         try {
-            String url = String.format("https://api.weixin.qq.com/wxa/getwxacode?access_token=%s",wx_access_token);
+            String url = String.format("%s/wxa/getwxacode?access_token=%s",host,wx_access_token);
             JsonObject post_body = (JsonObject) JSON.object2json(wx_body);
             return AJAX.download(url,"post",post_body.toString());
         } catch (Exception e) {
@@ -241,7 +246,7 @@ public class WeixinSDK implements WeixinAPI {
     @Override
     public byte[] wxa__getwxacodeunlimit(String wx_access_token,wxa__getwxacodeunlimit_body wx_body)throws WeixinError {
         try {
-            String url = String.format("https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=%s",wx_access_token);
+            String url = String.format("%s/wxa/getwxacodeunlimit?access_token=%s",host,wx_access_token);
             JsonObject post_body = (JsonObject) JSON.object2json(wx_body);
             return AJAX.download(url,"post",post_body.toString());
         } catch (Exception e) {
@@ -258,7 +263,7 @@ public class WeixinSDK implements WeixinAPI {
     public WeixinResponse cgi_bin__message__subscribe__send(String wx_access_token,subscribe__send_body wx_body) {
         JsonObject result;
         try {
-            String url = String.format("https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token=%s",wx_access_token);
+            String url = String.format("%s/cgi-bin/message/subscribe/send?access_token=%s",host,wx_access_token);
             JsonObject post_body = (JsonObject) JSON.object2json(wx_body);
             result = (JsonObject) JSON.parse(AJAX.request(url,"post",post_body.toString()));
         } catch (Exception e) {
